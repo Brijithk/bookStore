54c0175e54740e8cfb00e9d9bad6930bf230cf2b
Brijithk <105113563+Brijithk@users.noreply.github.com>
2024-10-12 08:28:52 +0530

    Add files via upload

--- bookStore/src/main/java/com/bookStore/BookStoreApplication.java
+++ bookStore/src/main/java/com/bookStore/BookStoreApplication.java
@@ -2,11 +2,14 @@ package com.bookStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BookStoreApplication {
-//	i have did it
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(BookStoreApplication.class, args);
 	}
 
--- bookStore/src/main/java/com/bookStore/controller/BookController.java
+++ bookStore/src/main/java/com/bookStore/controller/BookController.java
@@ -1,7 +1,22 @@
 package com.bookStore.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class BookController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+    @GetMapping("/book_register")
+    public String bookRegister() {
+        return "bookRegister";
+    }
+        @GetMapping("/available_books")
+        public String getAllBook() {
+            return "bookList";
+
+    }
 }
